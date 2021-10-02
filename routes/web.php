b34ba8ee1c8a6f5ab54c957a8ecd59afb19ace05
Ibrahim-Elsanhouri <ibrahimelsanhouri@gmail.com>
2021-10-02 23:42:19 +0300

    web.php fix pdf

--- routes/web.php
+++ routes/web.php
@@ -19,25 +19,10 @@ use App\Http\Controllers\ProjectController;
 
 Route::get('/', function () {
    return view('welcome');
-     // This  $data array will be passed to our PDF blade
-   //  $data = [
-     //   'title' => 'First PDF for Medium',
- /*       'heading' => 'Hello from 99Points.info',
-        'content' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.  standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.'        
-          ];
-      
-      $pdf = PDF::loadView('pdf_view', $data);  
-      return $pdf->download('medium.pdf'); */ 
+ 
 });
 
 
-Route::get('/pdf', function () {
-	$data = "ابراهيم";
-	$pdf = PDF::loadView('pdf.doc', compact('data'));
-	return $pdf->stream('document.pdf');
-  
-
-});
 Route::get('/invoice/{id}',[PaymentController::class, 'show'])->name('invoice');
 Route::get('/user/statement/{id}',[UserController::class, 'statement'])->name('user.statement');
 Route::get('/report',[ReportController::class, 'report'])->name('report');
